b9788236d39454440ec52927977b4641873278de
annemariemadsen <amma20@student.aau.dk>
2025-10-24 11:50:11 +0200

    more layers

--- MLP_classification.py
+++ MLP_classification.py
@@ -53,12 +53,14 @@ class SoftFilterNet(nn.Module):
     def __init__(self, input_size, num_filters, filter_dim, filters_tensor):
         super().__init__()
         self.fc1 = nn.Linear(input_size, 512)
-        self.fc2 = nn.Linear(512, num_filters)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, num_filters)
         self.register_buffer("filters", filters_tensor)  # fixed filters (not trainable)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        logits = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        logits = self.fc3(x)
         weights = F.softmax(logits, dim=1)            # [batch, num_filters]
         combined = torch.matmul(weights, self.filters) # [batch, filter_dim]
         return combined, weights
