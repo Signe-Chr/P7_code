d6f0bdbf2ced0401a5c95822f6c2780b1bbf4379
Signe-Chr <signe.smc@gmail.com>
2025-11-06 08:33:53 +0100

    changed NSDR to NSDP

--- Performance Evaluation/performance_evaluation_random_filter.py
+++ Performance Evaluation/performance_evaluation_random_filter.py
@@ -177,7 +177,7 @@ def compute_pesq_unfiltered(
 import torch
 import numpy as np
 
-def compute_nSDR(p_C: torch.Tensor, wav_input: torch.Tensor,
+def compute_nSDP(p_C: torch.Tensor, wav_input: torch.Tensor,
                              bright_zone_mics_index: list[int],
                              dark_zone_mics_index: list[int]):
     """
@@ -194,7 +194,7 @@ def compute_nSDR(p_C: torch.Tensor, wav_input: torch.Tensor,
     """
     ref = wav_input.squeeze().detach().cpu().numpy().astype(np.float32)
     
-    def compute_zone_nSDR(mic_indices):
+    def compute_zone_nSDP(mic_indices):
         nSDR_list = []
         for m in mic_indices:
             deg = p_C[m, :].detach().cpu().numpy().astype(np.float32)
@@ -203,8 +203,8 @@ def compute_nSDR(p_C: torch.Tensor, wav_input: torch.Tensor,
             s_target = ref[:min_len]
             s_est = deg[:min_len]
 
-            numerator = np.sum(s_target**2)
-            denominator = np.sum((s_target - s_est)**2)
+            denominator = np.sum(s_target**2)
+            numerator = np.sum((s_target - s_est)**2)
             if denominator == 0:
                 nSDR_val = 1e10
             else:
@@ -213,8 +213,8 @@ def compute_nSDR(p_C: torch.Tensor, wav_input: torch.Tensor,
         nSDR_list = np.array(nSDR_list)
         return np.mean(nSDR_list)
     
-    mean_B = compute_zone_nSDR(bright_zone_mics_index)
-    mean_D = compute_zone_nSDR(dark_zone_mics_index)
+    mean_B = compute_zone_nSDP(bright_zone_mics_index)
+    mean_D = compute_zone_nSDP(dark_zone_mics_index)
     
     return mean_B,mean_D
 
@@ -354,7 +354,7 @@ def average_performance_metrics_with_filters(RIR_test, selected_filters, wav_inp
 
     return results
 # Assuming `selected_filters_test` comes from your random selection
-results = average_performance_metrics_with_filters(RIRs_test, selected_filters, x_input, bright_zone_mics_index, dark_zone_mics_index)
+results = average_performance_metrics_with_filters(RIRs_test, filters_test, x_input, bright_zone_mics_index, dark_zone_mics_index)
 
 print(f"AC (mean, min, max): {results['AC']}")
 print(f"PESQ Bright Zone (mean, min, max): {results['PESQ_B']}")
