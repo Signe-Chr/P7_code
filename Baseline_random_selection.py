cd0d84f9c6c5439ca0568e2c9d61bab21b18ad0f
MaryHole <maryandrea2001@gmail.com>
2025-11-28 15:10:11 +0100

    push

--- Baseline_random_selection.py
+++ Baseline_random_selection.py
@@ -7,13 +7,13 @@ from Train_test_split import load_test_train_data
 data_test, data_train = load_test_train_data(test_size=0.25, random_seed=42)
 filters_test, filters_train = data_test[1], data_train[1]
 
-def random_selection(X_test, dictionary, seed_value):
+def random_selection(data_test, dictionary, seed_value):
     torch.manual_seed(seed_value)
     N_dic=dictionary.shape[0]
-    N_test=X_test.shape[0]
+    N_test=len(data_test)
     random_indices=torch.randint(low=0,high=N_dic,size=(N_test,))
     selected_filters=dictionary[random_indices]
-    return X_test, selected_filters, random_indices
+    return data_test, selected_filters, random_indices
 
 X_test, selected_filters, random_indices = random_selection(data_test, filters_train, 42)
 
