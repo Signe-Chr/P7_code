971a1e9b2694399f133084f3d84f02eac130b8fd
MaryHole <maryandrea2001@gmail.com>
2025-11-28 12:40:51 +0100

    Sidste filtre woop woop

--- RIR_generator.py
+++ RIR_generator.py
@@ -206,7 +206,7 @@ N = len(x_input)
 if __name__ == "__main__":
     save_path = "Data Archive"
     args = generate_configurations(rooms, RT60s, user_rotations, tilt_rotations, dark_mic_radius, z_height, M_D)
-    start = 140
+    start = 0
     for arg in tqdm(args[start:], initial=start, total=len(args)):
         (orientation_source_final, mic_positions_list, bright_zone_mics_index, dark_zone_mics_index,
                             x_input, RT60, mic_directions, user_rotation, fs_target, room_dim,
