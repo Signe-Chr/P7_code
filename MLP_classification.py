e7468c6097fdd4a94f68cd5a3d682002b9eabd1b
MaryHole <maryandrea2001@gmail.com>
2025-12-01 13:22:41 +0100

    .

--- MLP_classification.py
+++ MLP_classification.py
@@ -7,15 +7,40 @@ import Dataset_class as dc
 import Dataset_generator_script as dgs
 from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
+from Test_train_split import load_test_train_data
 
+"""
+def noget():
+    # Unpack data
+    a1, a2, a3, a4, a5, a6, a7, a8 = data
+    # Repack data to make sure it has the lengths it's supposed to
+    data = [a1, a2, np.array([a3]).T, np.array([a4]).T, a5, a6, a7, a8]
+    for epoch in range(epochs):
+        model.train()
+        total_loss = 0.0
 
-def train_epoch(model, dataloader, criterion, optimizer, device):
+        # Use zip(*data) to be able to loop over singular datapoints
+        for sample in zip(*data):   # <-- ét datapunkt ad gangen
+
+            # Pak datapunktet ud
+            X = torch.tensor(sample[0], dtype=torch.float32).to(dev)
+            flat_y = torch.tensor(sample[1], dtype=torch.float32).to(dev).unsqueeze(0)
+            IR = sample[5]    # eller sample[2], afhængigt af strukturen
+"""
+
+def train_epoch(model, data, criterion, optimizer, device):
     model.train()
     total_loss = 0.0
     correct = 0
     total = 0
 
-    loop = tqdm(dataloader, disable=not sys.stdout.isatty())
+    X_train = data[0] #Loads all the configurations
+    filters_train = data[1]
+    n_srcs_train = data[4]
+    RIRs_train = data[5]
+    indeces_train = data[6] #Loads the indeces for every filter in the dictionary
+
+    loop = tqdm(data, disable=not sys.stdout.isatty())
     for data in loop:
         X, y = data[0].to(device).float(), data[6].to(device)
 
@@ -40,27 +65,31 @@ def train_epoch(model, dataloader, criterion, optimizer, device):
 def main():
     # Device setup
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#
+    #from Dataset_generator_script import room_indices as ri
+    ## Data loading and setup
+    #data_dir = "Signes_data"
+    #a = os.listdir(data_dir)
+#
+#
+    #train_set = []
+    #for data in a:
+    #    r = int(data.split("_")[1])
+    #    if (r not in ri[::4]):
+    #        train_set.append(data)
+#
+#
+    #dataset = dc.CustomDataset(data_dir, train_set)
+    #input_size = len(dataset[0][0])
+    #output_size = len(dataset)
+#
+    #data_loader = DataLoader(dataset, batch_size=1, shuffle=False)
+    data_test, data_train = load_test_train_data()
 
-    from Dataset_generator_script import room_indices as ri
-    # Data loading and setup
-    data_dir = "Signes_data"
-    a = os.listdir(data_dir)
-
-
-    train_set = []
-    for data in a:
-        r = int(data.split("_")[1])
-        if (r not in ri[::4]):
-            train_set.append(data)
-
-
-    dataset = dc.CustomDataset(data_dir, train_set)
-    
-
-    data_loader = DataLoader(dataset, batch_size=1, shuffle=False)
-
-    input_size = len(dataset[0][0])
-    output_size = len(dataset)
+    x_input = torch.tensor([1])
+#
+    input_size = len(data_train[0][0])
+    output_size = len(data_train)
 
     # Model, loss, optimizer
     model = cvm.FilterNet_classification(input_size, output_size).to(device)
@@ -70,7 +99,7 @@ def main():
         model.load_state_dict(torch.load("MLP_classification.pth"))
     # Training loop
     for epoch in range(1, 41):
-        train_loss, train_acc = train_epoch(model, data_loader, criterion, optimizer, device)
+        train_loss, train_acc = train_epoch(model, data_train, criterion, optimizer, device)
         print(f"Epoch {epoch:02d} | Loss: {train_loss:.4f} | Acc: {train_acc:.2f}%")
 
     print("\n Training complete!")
