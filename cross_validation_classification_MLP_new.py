00a5d55b7c82e515da8a3a6ab62e07c92907b1af
MaryHole <maryandrea2001@gmail.com>
2025-12-04 10:21:39 +0100

    .

--- cross_validation_classification_MLP_new.py
+++ cross_validation_classification_MLP_new.py
@@ -7,7 +7,8 @@ import numpy as np
 from Loss_functions import Cosine_similarity, MSEP, AC_loss, MSE,compute_H_matrix
 import matplotlib.pyplot as plt
 
-p=3
+
+p=1
 neurons = [128, 256, 512]
 
 layers = [1,2,3]
@@ -15,7 +16,7 @@ layers = [1,2,3]
 num_folds=5
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-data_test,data_train=load_test_train_data()
+data_test, data_train, data_vail = load_test_train_data()
 
 
 input_size = len(data_train[0][0])
@@ -328,6 +329,7 @@ if p == 2:
     plot_mse_grid(axes[0], train_err_grid, "Train error")
     plot_mse_grid(axes[1], test_err_grid, "Test error")
     plt.tight_layout()
+    plt.savefig(f"CV_classification_2_layers.pdf")
     plt.show()
 
 if p == 3:
@@ -512,4 +514,6 @@ if p == 3:
     fig.colorbar(im_test, cax=cbar_ax_test, label='Test Loss')
 
     plt.tight_layout(rect=[0,0,0.9,1])  # leave space for colorbars
+    plt.savefig(f"CV_classification_3_layers.pdf")
     plt.show()
+
