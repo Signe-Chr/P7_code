74f670ba137930eed75a830212dfaa24c433c012
MaryHole <maryandrea2001@gmail.com>
2025-12-05 10:26:10 +0100

    Opdateret funktioner til at validere på både test og træning så man kan sammenligne

--- validation_accuracy.py
+++ validation_accuracy.py
@@ -37,29 +37,30 @@ def load_model(model_name):
 
 def validate_filters(model_name, X_test=X_test, Y_test=filters_test, X_train=X_train, Y_train=filters_train):
     model = load_model(model_name)
-    count = 0
-    difference_per_filter = 0
-    difference_per_entry = 0
-
 
     if model_name == "regression": # Test regression
         print("Testing on test data")
+        difference_per_filter = 0
+        difference_per_entry = 0
         for filter, configuration in zip(Y_test, X_test):
             with torch.no_grad():
                 output = model(configuration.unsqueeze(0).float())
-                difference = output-filter
+                difference = filter-output
                 difference_per_filter += (difference).sum()
                 difference_per_entry += (difference).mean()
 
         print(f"Average difference per entry: {difference_per_entry/len(X_test):.8f}.")        
         print(f"Average difference per filter: {difference_per_filter/len(X_test):.4f}.")
         print("")
+
+        difference_per_filter = 0
+        difference_per_entry = 0
         print("Testing on training data")
         for filter, configuration in zip(Y_train, X_train):
 
             with torch.no_grad():
                 output = model(configuration.unsqueeze(0).float())
-                difference = output-filter
+                difference = filter-output
                 difference_per_filter += (difference).sum()
                 difference_per_entry += (difference).mean()
 
@@ -67,10 +68,24 @@ def validate_filters(model_name, X_test=X_test, Y_test=filters_test, X_train=X_t
         print(f"Average difference per filter: {difference_per_filter/len(X_train):.4f}.")
 
     if model_name == "classification": # Test classification
+        print("Testing on test data")
+        count = 0
         for filter, configuration in zip(Y_test, X_test):
             with torch.no_grad():
                 output = model(configuration.unsqueeze(0).float())
-                output = _, prediction = torch.max(output, 1)
+                _, prediction = torch.max(output, 1)
+                output = Y_test[prediction]
+                if torch.all(filter == output):
+                    count += 1
+        print(f"{count}/{len(X_test)} correct.")
+        print(f"The model has an accuracy of {count/len(X_test)*100:.2f}%.")
+
+        count = 0
+        print("Testing on training data")
+        for filter, configuration in zip(Y_train, X_train):
+            with torch.no_grad():
+                output = model(configuration.unsqueeze(0).float())
+                _, prediction = torch.max(output, 1)
                 output = Y_train[prediction]
                 if torch.all(filter == output):
                     count += 1
@@ -78,7 +93,19 @@ def validate_filters(model_name, X_test=X_test, Y_test=filters_test, X_train=X_t
         print(f"The model has an accuracy of {count/len(X_test)*100:.2f}%.")
 
     elif model_name == "interpolation": # Test interpolation
+        count = 0
         for filter, configuration in zip(Y_test, X_test):
+            with torch.no_grad():
+                output = model(configuration.unsqueeze(0).float())
+                output = torch.matmul(output.float(), Y_test.float())
+                if torch.all(filter == output):
+                    count += 1
+        print(f"{count}/{len(X_test)} correct.")
+        print(f"The model has an accuracy of {count/len(X_test)*100:.2f}%.")
+
+        count = 0
+        print("Testing on training data")
+        for filter, configuration in zip(Y_train, X_train):
             with torch.no_grad():
                 output = model(configuration.unsqueeze(0).float())
                 output = torch.matmul(output.float(), Y_train.float())
