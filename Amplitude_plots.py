88037d4307560b652c231cf019dd44a14c1307c2
Signe-Chr <signe.smc@gmail.com>
2025-12-08 14:38:53 +0100

    some shit

--- Amplitude_plots.py
+++ Amplitude_plots.py
@@ -2,10 +2,11 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wavfile
-from Test_train_split import load_test_train_data
+from Test_train_split import load_test_train_data, x_input
 
 J=1024
-x_inp = [1] + [0]*(J+512-2)
+#x_inp = [1] + [0]*(J+512-2)
+x_inp=x_input.squeeze(0).numpy()
 data_test, data_train, data_val = load_test_train_data()
 
 q_filters=data_test[1][0]
@@ -91,9 +92,8 @@ plt.rcParams.update({"font.size": 17})
 # ------------------------
 ax1.plot(t, bright_original_signal, label='Unfiltered',
          color='limegreen', alpha=0.25, linewidth=1.2)
-ax1.plot(t, bright_signal, label='VAST',
+ax1.plot(t, bright_signal, label='ACC',
          color='orange', alpha=0.55, linewidth=1.2)
-ax1.set_xlim(0,0.028)
 
 ax1.set_ylabel("Normalised Amplitude", fontsize=20)
 ax1.set_title("Bright Zone Signals", fontsize=25)
@@ -108,10 +108,9 @@ ax1.legend(loc="upper right")
 # ------------------------
 ax2.plot(t, dark_original_signal, label='Unfiltered',
          color='limegreen', alpha=0.25, linewidth=1.2)
-ax2.plot(t, dark_signal, label='VAST',
+ax2.plot(t, dark_signal, label='ACC',
          color='blue', alpha=0.55, linewidth=1.2)
 
-ax2.set_xlim(0,0.028)
 ax2.set_xlabel("Time [s]", fontsize=20)
 ax2.set_ylabel("Normalised Amplitude", fontsize=20)
 ax2.set_title("Dark Zone Signals", fontsize=25)
