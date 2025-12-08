731534d8ea031e606f6596aa969758633899363e
MaryHole <maryandrea2001@gmail.com>
2025-12-08 14:48:34 +0100

    loss opdateret

--- Baseline_exhaustive_search.py
+++ Baseline_exhaustive_search.py
@@ -28,8 +28,10 @@ def Extensive_search(
     times_per_test = []
 
 
+    #Means: MSE: 1.189, Cosine similairty: 0.485, MSEP: 335.877, AC:19.689
+    #Medians: MSE: 0.002, Cosine similairty: 0.005, MSEP: 0.359, AC:3.437
     # Loss weights
-    lamda_mse, lambda_cosine, lambda_ac, lambda_msep = 1, 1, 1, 1 #0.002, 0.005, 0.009, 2.859  #1/3.729, 1/1.000, 1/18.390, 1/17.181
+    lamda_mse, lambda_cosine, lambda_ac, lambda_msep = 0.002, 0.005, 0.359, 3.437  #1/3.729, 1/1.000, 1/18.390, 1/17.181
     print("\nStarting Extensive Brute-Force Search with FULL COMPOSITE LOSS...")
     for i in tqdm(range(N_test)):
 
@@ -116,3 +118,5 @@ if __name__ == "__main__":
         max_filters=max_filters 
     )
 
+import winsound
+winsound.MessageBeep()
