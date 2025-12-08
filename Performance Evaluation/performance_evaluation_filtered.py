70369ee1e5a9d209cbc0a8ff1235bb03b6ea1812
Signe-Chr <signe.smc@gmail.com>
2025-12-08 16:15:54 +0100

    classification

--- Performance Evaluation/performance_evaluation_filtered.py
+++ Performance Evaluation/performance_evaluation_filtered.py
@@ -351,7 +351,7 @@ def loss_function_evaluation(RIR_test, selected_filters, wav_input, indeces_brig
 "classification"
 "acc"
 
-chosen_model = "acc"
+chosen_model = "classification"
 print(f"Du har valgt {chosen_model} til evaluering.")
 
 x_input = x_input
